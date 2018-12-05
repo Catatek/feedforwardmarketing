265826354ef0ce77d4328a94d13a381cc08cb366
William Whatley <william@catatek.com>
2018-12-05 10:08:17 -0500

    Added slider for mobile mocks.

--- src/components/shared/Mocks.jsx
+++ src/components/shared/Mocks.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Benefit from "./Benefit";
 import Browser from "../../assets/browser.png";
 import { Subtitle, Text, Column, Row } from "../../theme/index";
+import Slider from "react-slick";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -18,12 +19,6 @@ const Grid = styled.div`
   margin: 3em auto 2em auto;
   grid-template-columns: repeat(auto-fit, minmax(145px, 1fr));
   grid-auto-rows: 175px;
-  @media (max-width: 400px) {
-    grid-template-columns: repeat(2, 150px);
-    grid-auto-rows: 45px;
-    width: 95%;
-    grid-gap: 35px 10px;
-  }
 `;
 
 const StyledBox = styled.div`
@@ -47,20 +42,12 @@ const StyledBox = styled.div`
   &:hover {
     box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
   }
-  @media (max-width: 400px) {
-    flex-direction: row;
-    justify-content: space-around;
-    padding: 0.5em 0;
-  }
 `;
 
 const Icon = styled.i`
   font-size: 3em;
   transition: 750ms;
   color: ${props => (props.active ? "#00b2aa" : "#BFD1DA")};
-  @media (max-width: 400px) {
-    font-size: 1.75em;
-  }
 `;
 
 const boxes = [
@@ -121,69 +108,125 @@ export default class Mocks extends Component {
 
   render() {
     const { selected } = this.state;
+    let settings = {
+      dots: true,
+      infinite: false,
+      arrows: false,
+      speed: 1000
+    };
+
+    const mobile = window.matchMedia("(max-width: 780px)");
     return (
       <Wrapper>
         <Column alignitems="center" textalign="center">
           <Subtitle>Everything you need</Subtitle>
         </Column>
-        <Grid>
-          {boxes &&
-            boxes.map((key, index) => {
-              return (
-                <Box
-                  key={index}
-                  id={key.id}
-                  title={key.title}
-                  description={key.description}
-                  icon={key.icon}
-                  handleClick={this.handleClick}
-                  selected={selected}
+
+        {mobile.matches ? (
+          <div
+            style={{
+              margin: "1.5em 0"
+            }}
+          >
+            <Slider {...settings}>
+              <Benefit
+                img={Browser}
+                type="primary"
+                title="Effective"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+
+              <Benefit
+                img={Browser}
+                type="primary"
+                title="Efficient"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+
+              <Benefit
+                img={Browser}
+                type="primary"
+                title="Evaluations"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+
+              <Benefit
+                img={Browser}
+                type="primary"
+                title="To Do List"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+
+              <Benefit
+                img={Browser}
+                type="primary"
+                title="To Do List"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </Slider>
+          </div>
+        ) : (
+          <React.Fragment>
+            <Grid>
+              {boxes &&
+                boxes.map((key, index) => {
+                  return (
+                    <Box
+                      key={index}
+                      id={key.id}
+                      title={key.title}
+                      description={key.description}
+                      icon={key.icon}
+                      handleClick={this.handleClick}
+                      selected={selected}
+                    />
+                  );
+                })}
+            </Grid>
+            <Row>
+              {selected === "effective" && (
+                <Benefit
+                  img={Browser}
+                  type="primary"
+                  title="Effective"
+                  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
                 />
-              );
-            })}
-        </Grid>
-        <Row>
-          {selected === "effective" && (
-            <Benefit
-              img={Browser}
-              type="primary"
-              title="Effective"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            />
-          )}
-          {selected === "efficient" && (
-            <Benefit
-              img={Browser}
-              type="primary"
-              title="Efficient"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            />
-          )}
-          {selected === "evaluations" && (
-            <Benefit
-              img={Browser}
-              type="primary"
-              title="Evaluations"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            />
-          )}
-          {selected === "accountability" && (
-            <Benefit
-              img={Browser}
-              type="primary"
-              title="To Do List"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            />
-          )}
-          {selected === "analytical" && (
-            <Benefit
-              img={Browser}
-              type="primary"
-              title="To Do List"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            />
-          )}
-        </Row>
+              )}
+              {selected === "efficient" && (
+                <Benefit
+                  img={Browser}
+                  type="primary"
+                  title="Efficient"
+                  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+              )}
+              {selected === "evaluations" && (
+                <Benefit
+                  img={Browser}
+                  type="primary"
+                  title="Evaluations"
+                  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+              )}
+              {selected === "accountability" && (
+                <Benefit
+                  img={Browser}
+                  type="primary"
+                  title="To Do List"
+                  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+              )}
+              {selected === "analytical" && (
+                <Benefit
+                  img={Browser}
+                  type="primary"
+                  title="To Do List"
+                  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+              )}
+            </Row>
+          </React.Fragment>
+        )}
       </Wrapper>
     );
   }
