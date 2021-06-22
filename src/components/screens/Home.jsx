42e08edfb671ca583e3e5873e6f060543682606a
William <william@williamwhatley.io>
2021-06-22 12:27:39 -0400

    ..

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -12,40 +12,40 @@ import Footer from "../shared/Footer";
 const values = [
   {
     icon: "fal fa-user-md",
-    title: "Select your Seciality",
+    title: "Select your Specialty",
     description:
-      "To find your evaluators, select your program, specialty, rotation, and year."
+      "To find your evaluators, select your program, specialty, rotation, and year.",
   },
   {
     icon: "fal fa-clipboard-list-check",
     title: "To-do List",
     description:
-      "Quickly view your clinical rotation requirements and what proficiencies are expected."
+      "Quickly view your clinical rotation requirements and what proficiencies are expected.",
   },
   {
     icon: "fal fa-handshake-alt",
     title: "Collaborate",
     description:
-      "Collaborate with evaluators by requesting instant evaluations during your rotation."
+      "Collaborate with evaluators by requesting instant evaluations during your rotation.",
   },
   {
     icon: "fal fa-tachometer-alt",
     title: "Admin Dashboard",
     description:
-      "Easily input rotation tasks for various sets of users and see how your organization is performing."
+      "Easily input rotation tasks for various sets of users and see how your organization is performing.",
   },
   {
     icon: "fal fa-star",
     title: "Request Reviews",
     description:
-      "Once you have completed a task, with one simple click, select an evaluator for your approval."
+      "Once you have completed a task, with one simple click, select an evaluator for your approval.",
   },
   {
     icon: "fal fa-bell",
     title: "Notifications",
     description:
-      "Instantly view invaluable feedback from evaluators when they review your requests and submit."
-  }
+      "Instantly view invaluable feedback from evaluators when they review your requests and submit.",
+  },
 ];
 
 const student = [
@@ -53,20 +53,20 @@ const student = [
     id: "clinical",
     title: "Understand your clinical tasks",
     description: "when you show up for your rotation.",
-    icon: "fal fa-bullseye-arrow"
+    icon: "fal fa-bullseye-arrow",
   },
   {
     id: "request",
     title: "Request on-demand reviews",
     description: "by selecting your evaluator on the spot.",
-    icon: "fal fa-clipboard-list-check"
+    icon: "fal fa-clipboard-list-check",
   },
   {
     id: "review",
     title: "Review invaluable feedback",
     description: "and understand exactly how to improve.",
-    icon: "fal fa-chart-bar"
-  }
+    icon: "fal fa-chart-bar",
+  },
 ];
 
 const organization = [
@@ -74,20 +74,20 @@ const organization = [
     id: "add",
     title: "Add clinical tasks",
     description: "to any combination of student programs.",
-    icon: "fal fa-tasks"
+    icon: "fal fa-tasks",
   },
   {
     id: "accountable",
     title: "Hold faculty accountable",
     description: "by viewing task reviews.",
-    icon: "fal fa-handshake-alt"
+    icon: "fal fa-handshake-alt",
   },
   {
     id: "insight",
     title: "Gain invaluable insight",
     description: "into your organization's health.",
-    icon: "fal fa-search"
-  }
+    icon: "fal fa-search",
+  },
 ];
 
 export default function Home() {
